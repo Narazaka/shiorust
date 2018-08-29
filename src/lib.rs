f70f52480de1ba4cac22f0fa7c9b302006d5955a
Narazaka <info@narazaka.net>
2018-08-29 20:01:40 +0900

    lazy-tmp

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate regex;
 use regex::Regex;
 
@@ -21,34 +23,35 @@ pub enum ParseErrorKind {
     HeaderLine,
 }
 
-const CRLF: &'static str = "a";
-
-static REQUEST_LINE_RE: Regex = Regex::new(r"").unwrap();
-static HEADER_RE: Regex = Regex::new(r"").unwrap();
-
-#[derive(Debug)]
-pub struct Headers {
-    headers: HashMap<String, String>,
+const CRLF: &'static str = "\r\n";
+lazy_static! {
+    static ref REQUEST_LINE_RE: Regex = Regex::new(r"").unwrap();
+    static ref HEADER_RE: Regex = Regex::new(r"").unwrap();
 }
 
-impl Headers {
-    pub fn new(headers: HashMap<String, String>) -> Headers {
+#[derive(Debug)]
+pub struct Headers<'a> {
+    headers: &'a HashMap<&'a str, &'a str>,
+}
+
+impl<'a> Headers<'a> {
+    pub fn new(headers: &'a HashMap<&str, &str>) -> Headers<'a> {
         Headers { headers: headers }
     }
-    pub fn header(&self, name: String) -> Option<&String> {
-        self.headers.get(&name)
+    pub fn header(&self, name: &str) -> Option<&&str> {
+        self.headers.get(name)
     }
 }
 
 #[derive(Debug)]
-pub struct Request {
-    pub method: String,
-    pub version: String,
-    pub headers: Headers,
+pub struct Request<'a> {
+    pub method: &'a str,
+    pub version: &'a str,
+    pub headers: Headers<'a>,
 }
 
-impl Request {
-    pub fn parse(request_str: String) -> Result<Request, ParseError> {
+impl<'a> Request<'a> {
+    pub fn parse(request_str: &str) -> Result<Request, ParseError> {
         use self::ParseErrorKind::*;
 
         let lines = request_str.split(CRLF);
@@ -61,7 +64,7 @@ impl Request {
         } else {
             return Err(ParseError { kind: InvalidLength });
         };
-        let headers = HashMap::new();
+        let mut headers = HashMap::new();
         let header_lines = lines.skip(1);
         for header_line in header_lines {
             if let Some(captures) = HEADER_RE.captures(header_line) {
@@ -74,11 +77,11 @@ impl Request {
         Ok(Request {
             method: method,
             version: version,
-            headers: Headers::new(headers),
+            headers: Headers::new(&headers),
         })
     }
 
-    pub fn header(&self, name: String) -> Option<&String> {
+    pub fn header(&self, name: &str) -> Option<&&str> {
         self.headers.header(name)
     }
 }
